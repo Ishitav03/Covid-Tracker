068da2897b42e2dae1b326dd76ee548561206472
Ishitav03 <vishira383@gmail.com>
2021-12-03 00:58:28 +0530

    Updates

---
+++ precache-manifest.cb69967aedf7777bcf85864983c55875.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26fcbdec2b5a8c98a4a9b740dd58fa97",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "d9c4bba6b69942e7fb18",
+    "url": "/Covid-Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "dce8d731be7fed41bd56",
+    "url": "/Covid-Tracker/static/css/main.84b4e540.chunk.css"
+  },
+  {
+    "revision": "d9c4bba6b69942e7fb18",
+    "url": "/Covid-Tracker/static/js/2.c3990831.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid-Tracker/static/js/2.c3990831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce8d731be7fed41bd56",
+    "url": "/Covid-Tracker/static/js/main.42caf9fb.chunk.js"
+  },
+  {
+    "revision": "ae45445b70030a69d951",
+    "url": "/Covid-Tracker/static/js/runtime-main.d4cdb983.js"
+  }
+]);
